0030b9631991bad064acb9e78cc78ec8b9155cf9
ryouta <ryouta.k1982@gmail.com>
2021-04-18 20:59:29 +0900

    Modify templates

--- Documents/Python Scripts/django_first/blog/models.py
+++ Documents/Python Scripts/django_first/blog/models.py
@@ -17,4 +17,5 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+
         
--- Documents/Python Scripts/django_first/blog/views.py
+++ Documents/Python Scripts/django_first/blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index.")
